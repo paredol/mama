51b6f4f17e0eb56e86665aea77ebddfbf7bc7464
Aite Aigbe <aite@nabla.ooo>
2022-12-02 16:40:56 -0500

    signup done

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: { include: ['firebase/app', 'firebase/firestore'] },
   plugins: [react()]
 })
